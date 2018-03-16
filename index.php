b2bc35d874a943db9a22375992af82f5462ff47d
Brasgaard <alexanderbrasgaard@hotmail.com>
2018-03-16 10:43:28 +0100

    Folders, Index, CSS, JS
    Create the different folders we need alongside with the index, css and js file.

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Title -->
+	<title>Grundfos Project</title>
+	
+	<!-- Description -->
+	<meta charset="UTF-8">
+	<meta name="Description" content="Insert text here">
+
+	<!-- Keywords -->
+	<meta name="keywords" content="HTML,CSS,JavaScript">
+
+	<!-- Viewport -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Css, Sass and fonts-->
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+
+    <!-- Favicons -->
+	<link rel="icon" type="image/png" href="#" sizes="32x32" />
+	<link rel="icon" type="image/png" href="#" sizes="16x16" />
+</head>
+<body>
+
+
+<!-- Scripts -->
+<script src="js/scripts.js"></script>
+</body>
+</html>
