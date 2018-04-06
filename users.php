8766746d6bd5e8c63cfdf64d83f7d7a3a21b8559
LukasBrondel <lukas_broendel@hotmail.com>
2018-04-06 13:10:40 +0200

    Added menu icons + moved style.css into the css folder

--- users.php
+++ users.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Userlist</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="scss/style.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
     <link href="css/fontawesome-all.min.css" rel="stylesheet">
     <script src="main.js"></script>
 </head>
@@ -18,9 +18,9 @@
 
         <div class="gridContainer__div rightNavMenu">
             <a href="index.php" class="rightNavMenu__a"><i class="fas fa-home"></i> Home</a>
-            <a href="users.php" class="rightNavMenu__a">Users</a>
-            <a href="rewards.php" class="rightNavMenu__a">Rewards</a>
-            <a href="diplomas.php" class="rightNavMenu__a">Diplomas</a>
+            <a href="users.php" class="rightNavMenu__a"><i class="fas fa-user"></i> Users</a>
+            <a href="rewards.php" class="rightNavMenu__a"><i class="fas fa-gift"></i> Rewards</a>
+            <a href="diplomas.php" class="rightNavMenu__a"><i class="fas fa-clipboard-check"></i> Diplomas</a>
         </div>
 
         <div class="gridContainer__div mainContent">
