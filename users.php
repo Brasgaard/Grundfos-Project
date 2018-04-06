246a6036dc39304e6d1c12b17ea55a482905eac0
FAROUTPC\Sean <sean.rcordero@gmail.com>
2018-04-06 12:19:50 +0200

    Layout template in SCSS and fonts

---
+++ users.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Userlist</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="scss/style.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <div class="gridContainer">
+
+        <header class="gridContainer__div grundfosHeader">
+            <p>Test</p>
+        </header>
+
+        <div class="gridContainer__div rightNavMenu">
+            <a href="index.php" class="rightNavMenu__a">Home</a>
+            <a href="users.php" class="rightNavMenu__a">Users</a>
+            <a href="rewards.php" class="rightNavMenu__a">Rewards</a>
+            <a href="diplomas.php" class="rightNavMenu__a">Diplomas</a>
+        </div>
+
+        <div class="gridContainer__div mainContent">
+            <p>Test</p>
+        </div>
+
+        <footer class="gridContainer__div grundfosFooter">
+            <p>Test</p>
+        </footer>
+
+    </div>    
+</body>
+</html>
