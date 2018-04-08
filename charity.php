739eeb872f8387cec5c8fbda7503262b64eced60
Brasgaard <alexanderbrasgaard@hotmail.com>
2018-04-09 00:17:13 +0200

    Reward, chairty, product
    - Added 2 new pages for the products and chairty pages.
    - Fixed 3 pictures dimensions to 510x450
    - Made the reward page
    - Charity site almost done (Needs Charity title)

---
+++ charity.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Userlist</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="scss/style.css" />
+    <link href="css/fontawesome-all.min.css" rel="stylesheet">
+    <script src="main.js"></script>
+</head>
+<body>
+    <div class="gridContainer">
+
+        <header class="gridContainer__div grundfosHeader">
+            <p>Test</p>
+        </header>
+
+        <div class="gridContainer__div rightNavMenu">
+            <a href="index.php" class="rightNavMenu__a"><i class="fas fa-home"></i> Home</a>
+            <a href="users.php" class="rightNavMenu__a"><i class="fas fa-user"></i> Users</a>
+            <a href="rewards.php" class="rightNavMenu__a"><i class="fas fa-gift"></i> Rewards</a>
+            <a href="diplomas.php" class="rightNavMenu__a"><i class="fas fa-clipboard-check"></i> Diplomas</a>
+        </div>
+
+        <div class="gridContainer__div mainContentCharity">
+            
+            <!-- THE WATER PROJECT CHARITY -->
+            <section class="charitypics">
+                <img class="charitypictures" src="img/grundfos-charity.jpg">
+            </section>
+                
+            <section class="charitytext">
+                <h3 class="titletextcharity">The Water Project - Water to the children of Africa</h3>
+                <p class="bodytextcharity">The Water Project is a charity that provides access to clean, safe and reliable water and sanitation solutions across sub-Saharan Africa, one village at a time.<br><br>
+
+                Finding water is a daily challenge for young girls, moms and sons. With a charitable donation today, you can lift this burden.
+                Providing a reliable and safe water source will unlock potential by returning time for study, work, and imagination. <br><br>
+
+                A gift of lasting opportunity.</p>
+
+                <a href="#"><button class="donatebtn" type="button">Donate</button></a>
+                <a href="https://thewaterproject.org/" target="_blank"><button class="readmorebtn" type="button">Read more</button></a>
+             </section>
+
+            <!-- WWF CHARITY -->
+            <section class="charitypics">
+                <img class="charitypictures" src="img/grundfos-wwf.jpg">
+            </section>
+            
+            <section class="charitytext">
+                <h3 class="titletextcharity">WWF - Protect nature and enviroment</h3>
+                <p class="bodytextcharity">Our mission is to conserve nature and reduce the most pressing threats to the diversity of life on Earth.<br><br>
+
+                WWF's unique way of working combines global reach with a foundation in science, involves action at every level from local to global, and ensures the delivery of innovative solutions that meet the needs of both people and nature.<br><br>
+
+                Together we can make a differfence.</p>
+
+                <a href="#"><button class="donatebtn" type="button">Donate</button></a>
+                <a href="https://www.worldwildlife.org/" target="_blank"><button class="readmorebtn" type="button">Read more</button></a>
+             </section>
+
+        </div>
+
+        <footer class="gridContainer__div grundfosFooter">
+            <p>Test</p>
+        </footer>
+
+</body>
+</html>
--- product.php
+++ product.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Grundfos - Reward</title>
+    <title>Userlist</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="scss/style.css" />
     <link href="css/fontawesome-all.min.css" rel="stylesheet">
@@ -23,28 +23,8 @@
             <a href="diplomas.php" class="rightNavMenu__a"><i class="fas fa-clipboard-check"></i> Diplomas</a>
         </div>
 
-        <div class="gridContainer__div mainContentReward">
-            <section id="product">
-                <p class="producttitle">
-                    Product
-                </p>
-
-                <img class="rewardpictures" src="img/grundfos-product.jpg" title="grundfos product rewards" alt="grundfos product rewards">
-                <br>
-                <a href="product.php"><button class="readmorebtn" type="button">Read more</button></a>
-            
-            </section>
-                
-            <section id="charity">
-                <p class="charitytitle">
-                    Charity
-                </p>
-
-                <img class="rewardpictures" src="img/grundfos-charity.jpg" title="grundfos support a charity" alt="grundfos support a charity">
-                <br>
-                <a href="charity.php"><button class="readmorebtn" type="button">Read more</button></a>
-
-            </section>
+        <div class="gridContainer__div mainContent">
+            <p>Test test test</p>
         </div>
 
         <footer class="gridContainer__div grundfosFooter">
