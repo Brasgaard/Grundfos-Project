f3005a95f37c92384a41d75c797e6c6a3e1c8a0b
LukasBrondel <lukas_broendel@hotmail.com>
2018-04-08 14:44:37 +0200

    Menu icons on index.php

--- index.php
+++ index.php
@@ -1,31 +1,35 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<!-- Title -->
-	<title>Grundfos Project</title>
-	
-	<!-- Description -->
-	<meta charset="UTF-8">
-	<meta name="Description" content="Insert text here">
-
-	<!-- Keywords -->
-	<meta name="keywords" content="HTML,CSS,JavaScript">
-
-	<!-- Viewport -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Userlist</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<!-- Css, Sass and fonts-->
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-
-
-    <!-- Favicons -->
-	<link rel="icon" type="image/png" href="#" sizes="32x32" />
-	<link rel="icon" type="image/png" href="#" sizes="16x16" />
+    <link rel="stylesheet" type="text/css" media="screen" href="scss/style.css" />
+    <link href="css/fontawesome-all.min.css" rel="stylesheet">
+    <script src="main.js"></script>
 </head>
 <body>
+    <div class="gridContainer">
 
+        <header class="gridContainer__div grundfosHeader">
+            <p>Test</p>
+        </header>
 
-<!-- Scripts -->
-<script src="js/scripts.js"></script>
+        <div class="gridContainer__div rightNavMenu">
+            <a href="index.php" class="rightNavMenu__a"><i class="fas fa-home"></i> Home</a>
+            <a href="users.php" class="rightNavMenu__a"><i class="fas fa-user"></i> Users</a>
+            <a href="rewards.php" class="rightNavMenu__a"><i class="fas fa-gift"></i> Rewards</a>
+            <a href="diplomas.php" class="rightNavMenu__a"><i class="fas fa-clipboard-check"></i> Diplomas</a>
+        </div>
+
+        <div class="gridContainer__div mainContent">
+            <p>Test</p>
+        </div>
+
+        <footer class="gridContainer__div grundfosFooter">
+            <p>Test</p>
+        </footer>
+
 </body>
 </html>
