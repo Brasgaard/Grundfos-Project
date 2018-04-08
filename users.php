b5d78797e684f92b17a62f3bfd40f03de482fcff
FAROUTPC\Sean <sean.rcordero@gmail.com>
2018-04-08 20:35:56 +0200

    SCSS, buttons and table

--- users.php
+++ users.php
@@ -15,21 +15,56 @@
             <p>Test</p>
         </header>
 
-        <div class="gridContainer__div rightNavMenu">
-            <a href="index.php" class="rightNavMenu__a">Home</a>
-            <a href="users.php" class="rightNavMenu__a">Users</a>
-            <a href="rewards.php" class="rightNavMenu__a">Rewards</a>
-            <a href="diplomas.php" class="rightNavMenu__a">Diplomas</a>
+        <div class="gridContainer__div leftNavMenu">
+            <a href="index.php" class="leftNavMenu__a">Home</a>
+            <a href="manageusers.php" class="leftNavMenu__a">Users</a>
+            <a href="rewards.php" class="leftNavMenu__a">Rewards</a>
+            <a href="diplomas.php" class="leftNavMenu__a">Diplomas</a>
         </div>
 
         <div class="gridContainer__div mainContent">
-            <p>Test</p>
+            <h1 class="mainContent__h1">Manage your employees within the Grundfos Ecademy</h1>
+            <p class="mainContent__p">Add users and assign courses, or check the stadings of current users within your organization.</p>
+            <button id="addBtn">Add User</button>
+            <button id="courseBtn">Assign Course</button>
+            <button id="removeBtn">Remove User</button>
+            <table class="mainContent__userList">
+                <tr id="mainListRow">
+                    <th><input type="checkbox" name="" id="mainCheck">Select all</th>
+                    <th>Name</th>
+                    <th>Role</th>
+                    <th>Points</th>
+                    <th>Courses</th>
+                </tr>
+                <tr class="userRow">
+                    <td><input type="checkbox" name="userCheck" id=""></td>
+                    <td>Lukas Brøndel</td>
+                    <td>Wholesaler</td>
+                    <td>5000</td>
+                    <td>Magna 2</td>
+                </tr>
+                <tr class="userRow">
+                    <td><input type="checkbox" name="userCheck" id=""></td>
+                    <td>Alexander Braasgard</td>
+                    <td>Engineer</td>
+                    <td>7000</td>
+                    <td>Pumps</td>
+                </tr>
+                <tr class="userRow">
+                    <td><input type="checkbox" name="userCheck" id=""></td>
+                    <td>Sean Ryg</td>
+                    <td>Commercial Builder</td>
+                    <td>10000</td>
+                    <td>Heaters</td>
+                </tr>
+            </table>
         </div>
 
         <footer class="gridContainer__div grundfosFooter">
             <p>Test</p>
         </footer>
 
-    </div>    
+    </div>
+<script src="js/userlist.js"></script>    
 </body>
 </html>
