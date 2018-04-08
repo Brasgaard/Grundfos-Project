1b336ed4ab1f390fe919e5fa2e4a2986e719b257
Brasgaard <alexanderbrasgaard@hotmail.com>
2018-04-09 01:11:28 +0200

    Buttons, images and diploma page
    - Added the diploma page
    - Fixed 2 images dimensions to 510x450
    - Facebook and Linkedin buttons done

---
+++ diplomas.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Grundfos Ecademy - Charity</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="scss/style.css" />
+    <link href="css/fontawesome-all.min.css" rel="stylesheet">
+    <script src="main.js"></script>
+</head>
+<body>
+    <div class="gridContainer">
+
+        <header class="gridContainer__div grundfosHeader">
+            <p>Test</p>
+        </header>
+
+        <div class="gridContainer__div leftNavMenu">
+            <a href="index.php" class="leftNavMenu__a"><i class="fas fa-home"></i> Home</a>
+            <a href="users.php" class="leftNavMenu__a"><i class="fas fa-user"></i> Users</a>
+            <a href="rewards.php" class="leftNavMenu__a"><i class="fas fa-gift"></i> Rewards</a>
+            <a href="diplomas.php" class="leftNavMenu__a"><i class="fas fa-clipboard-check"></i> Diplomas</a>
+        </div>
+
+        <div class="gridContainer__div mainContentCharity">
+            
+            <!-- WASTEWATER DIPLOMA -->
+            <section class="charitypics">
+                <img class="charitypictures" src="img/grundfos-wastewater.jpg">
+            </section>
+                
+            <section class="charitytext">
+                <h3 class="titletextcharity">Municipal Wastewater</h3>
+                <p class="bodytextcharity">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br><br>
+
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. <br><br>
+
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                <a href="#"><button class="donatebtn" type="button">Download</button></a>
+                <a href="#" target="_blank"><button class="facebookbtn" type="button"><i class="fab fa-facebook-f"></i> Share</button></a>
+                <a href="#" target="_blank"><button class="linkedinbtn" type="button"><i class="fab fa-linkedin-in"></i> Share</button></a>
+             </section>
+
+            <!-- COMMERCIAL BUILDING -->
+            <section class="charitypics">
+                <img class="charitypictures" src="img/grundfos-commercial.jpg">
+            </section>
+            
+            <section class="charitytext">
+                <h3 class="titletextcharity">Commercial Building</h3>
+                <p class="bodytextcharity">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br><br>
+
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. <br><br>
+
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                <a href="#"><button class="donatebtn" type="button">Download</button></a>
+                <a href="#" target="_blank"><button class="facebookbtn" type="button"><i class="fab fa-facebook-f"></i> Share</button></a>
+                <a href="#" target="_blank"><button class="linkedinbtn" type="button"><i class="fab fa-linkedin-in"></i> Share</button></a>
+             </section>
+
+        </div>
+
+        <footer class="gridContainer__div grundfosFooter">
+            <p>Test</p>
+        </footer>
+
+</body>
+</html>
