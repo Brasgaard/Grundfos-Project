c2f8ab6c7fa7d5f713d07bcb2f4836a99808b73a
LukasBrondel <lukas_broendel@hotmail.com>
2018-04-09 13:52:51 +0200

    Index.php recent activity and diploma progress done

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Userlist</title>
+    <title>Ecademy - Home</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="scss/style.css" />
 <link href="css/fontawesome-all.min.css" rel="stylesheet">
@@ -32,7 +32,7 @@
                 <h2>Share your latest achievements on LinkedIn</h2>
                 <p><strong>Erhvervsakademi Aarhus</strong> has unlocked the diploma <strong>Commercial Buildings</strong>.<br/>
                 Do you want to share this with your network?<br/>
-            
+                
                 <a href="http://www.linkedin.com"><img src="img/linkedin-share.png" alt="Share on LinkedIn" class="linkedIn"></a>
                 </p>
             </article>
@@ -47,19 +47,27 @@
                 </article>
                 <article class="stats__diplomaProgress">
                     <h3>Diploma Progress</h3>
-                    <p class="diploma-name">Water something</p>
+                    <p class="diploma-name">Municipal Wastewater</p>
                     <div class="diploma-container">
                     	<div class="diplomaRequirement municipalWastewater"><i class="fas fa-user"></i> 2 / 4</div>
                     </div>
-                    <p class="diploma-name">Water something</p>
+                    <p class="diploma-name">Commercial Buildings</p>
                     <div class="diploma-container">
-                    	<div class="diplomaRequirement municipalWastewater"><i class="fas fa-user"></i> 2 / 4</div>
+                    	<div class="diplomaRequirement commercialBuildings"><i class="fas fa-user"></i> 3 / 4</div>
                     </div>
-                    <p class="diploma-name">Water something</p>
+                    <p class="diploma-name">Industry</p>
                     <div class="diploma-container">
-                    	<div class="diplomaRequirement municipalWastewater"><i class="fas fa-user"></i> 2 / 4</div>
+                    	<div class="diplomaRequirement"><i class="fas fa-user"></i> 4 / 4</div>
                     </div>
                 </article>
+            </section>
+            <section class="activityFeed">
+                <h3>Recent Activity</h3>
+                <ul>
+                    <li><i class="fas fa-user"></i> <strong>Lukas Brøndel</strong> finished the task <strong>Life Cycle Costs</strong>.<br/><small>April 4, 2018</small></li>
+                    <li><i class="fas fa-clipboard-check"></i> <strong>Erhvervsakademi Aarhus</strong> has unlocked the diploma <strong>Industry</strong>.<br/><small>April 1, 2018</small></li>
+                    <li><i class="fas fa-user"></i> <strong>Alexander Brasgaard</strong> has been added to <strong>Erhvervsakademi Aarhus</strong>.<br/><small>March 27, 2018</small></li>
+                </ul>
             </section>
         </div>
 
